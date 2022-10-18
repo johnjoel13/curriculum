7254f2b31d0fd4d78a6936e558d3391b66c4584b
johnjoel13 <akihirokato666@icloud.com>
2022-10-18 19:23:52 +0900

    4-1チェックテスト

--- JAVA_STUDY/4-1/4-1/4-1/src/check/Main.java
+++ JAVA_STUDY/4-1/4-1/4-1/src/check/Main.java
@@ -1,14 +1,19 @@
 package check;
+
+import constants.Constants;
+
 public class Main {
 	    private static String firstName = "加藤";
 		private static String lastName = "聖弘";
 	private static void printName(String firstName,String lastName) {
+	System.out.println("printNameメソッド　⇨ " +firstName+lastName);
 	}
+	
 	public static void main(String[] args) {
-		System.out.println("printNameメソッド　→ " +firstName+lastName);
-	Pet pt = new Pet("java吉", "hoge");
+	printName(firstName,lastName);
+	Pet pt = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
 		pt.introduce();
-	RobotPet rp = new RobotPet("R2D2","ルーク");
+	RobotPet rp = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
 		rp.introduce();
 	
 }
