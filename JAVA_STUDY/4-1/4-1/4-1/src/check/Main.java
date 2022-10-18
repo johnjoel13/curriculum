31eb972599f7cf4659258ed085b9c6126a8a3a11
johnjoel13 <akihirokato666@icloud.com>
2022-10-18 19:11:59 +0900

    チェックテスト

---
+++ JAVA_STUDY/4-1/4-1/4-1/src/check/Main.java
@@ -0,0 +1,15 @@
+package check;
+public class Main {
+	    private static String firstName = "加藤";
+		private static String lastName = "聖弘";
+	private static void printName(String firstName,String lastName) {
+	}
+	public static void main(String[] args) {
+		System.out.println("printNameメソッド　→ " +firstName+lastName);
+	Pet pt = new Pet("java吉", "hoge");
+		pt.introduce();
+	RobotPet rp = new RobotPet("R2D2","ルーク");
+		rp.introduce();
+	
+}
+}
---
+++ JAVA_STUDY/4-1/check/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author leechanyeong
+ *
+ */
+module check {
+}
